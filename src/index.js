95fd18c2d248abfd5d8e2e862bc600b83e7d1f89
david63011 <david630@gmail.com>
2022-11-17 23:42:21 -0300

    lintors check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable comma-dangle */
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
