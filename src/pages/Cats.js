50b174f25734405da33a1eeafc74d94f340d60d4
david63011 <david630@gmail.com>
2022-11-18 09:09:27 -0300

    lintors

--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -1,11 +1,7 @@
-import React from 'react';
-
-const Cats = () => {
-  return (
-    <div className="cats">
-      <button className="checkbtn">Check Status</button>
-    </div>
-  );
-};
+const Cats = () => (
+  <div className="cats">
+    <button className="checkbtn">Check Status</button>
+  </div>
+);
 
 export default Cats;
