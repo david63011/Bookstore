aba3e4aabeb9724f9ba2fa0c49df186734dfb113
david63011 <david630@gmail.com>
2022-11-18 09:58:35 -0300

    add lintors

--- src/component/Books.js
+++ src/component/Books.js
@@ -1,42 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 const Books = ({ title, author }) => (
-    <div className="bookSite">
-      <div className="book-info">
-        <ul>
-          <li className="genre books">Action</li>
-          <div>
-            <li className="booktitle books">{title}</li>
-          </div>
-          <li className="bookauthor books">{author}</li>
-        </ul>
+  <div className="bookSite">
+    <div className="book-info">
+      <ul>
+        <li className="genre books">Action</li>
+        <div>
+          <li className="booktitle books">{title}</li>
+        </div>
+        <li className="bookauthor books">{author}</li>
+      </ul>
 
-        <div className="book-actions">
-          <button className="book-btn" type="button">
-            Comments
-          </button>
-          <button className="book-btn" type="button">
-            Remove
-          </button>
-          <button className="book-btn" type="button">
-            Edit
-          </button>
-        </div>
-        <div className="progress">
-          <progress value="60" max="100">
-            {' '}
-            60%
-          </progress>
-          <span>60%</span>
-        </div>
+      <div className="book-actions">
+        <button className="book-btn" type="button">
+          Comments
+        </button>
+        <button className="book-btn" type="button">
+          Remove
+        </button>
+        <button className="book-btn" type="button">
+          Edit
+        </button>
+      </div>
+      <div className="progress">
+        <progress value="60" max="100">
+          {' '}
+          60%
+        </progress>
+        <span>60%</span>
       </div>
     </div>
-  );
+  </div>
+);
 
 Books.propTypes = {
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired
+  author: PropTypes.string.isRequired,
 };
 
 export default Books;
