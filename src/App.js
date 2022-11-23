23230ec4eaee1b98d866fd2b33b0a6499842a4ed
david63011 <david630@gmail.com>
2022-11-23 12:38:57 -0300

    add lintors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import React from 'react';
-import Navbar from './Navbar';
 import Firstbook from './pages/Firstbook';
 import Cats from './pages/Cats';
 import { Provider } from 'react-redux';
 import { store } from './component/redux/configureStore';
+import Navbar from './Navbar';
 
 function App() {
   return (
