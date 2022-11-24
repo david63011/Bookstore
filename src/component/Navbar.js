2428b5b356747ee83735c2968a5d7e4b0f758d2d
david63011 <david630@gmail.com>
2022-11-24 12:05:59 -0300

    add changes

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => 
     <nav className="navbar">
       <h1 className="logo">
-        <Link to="/">David's Bookstore</Link>
+        <Link to="/">David's Cool Bookstore</Link>
       </h1>
       <ul className="nav-links">
         <li>
