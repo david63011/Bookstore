d90cb88c6f8a222b75d7093a87ba2b8e77033a7c
david63011 <david630@gmail.com>
2022-11-24 10:05:53 -0300

    add

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => 
     <nav className="navbar">
       <h1 className="logo">
-        <Link to="/">Bookstore</Link>
+        <Link to="/">The Bookstore</Link>
       </h1>
       <ul className="nav-links">
         <li>
